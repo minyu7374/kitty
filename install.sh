6d97bdad6b022b825abfddee8b8ecb4a25a04df0
minyu <minyu@outlook.com>
2025-09-05 16:53:32 +0800

    install 脚本完善

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ find "$code_dir" -maxdepth 1 -name "*.conf" ! -name "*-macos.conf" -print0 | xar
 cp -f "$code_dir"/kitty.sh ~/.sh.d/
 
 if [[ "$(uname -s)" == Darwin ]]; then
-    ln -sf "$code_dir"/*-macos.conf .
-    ln -sf kitty-ext{-macos,}.conf
-    ln -sf open-actions{-macos,}.conf 
+    for conf_name in kitty-ext open-actions; do
+        ln -sf {"$code_dir/${conf_name}-macos",$conf_name}.conf
+    done
 else
     ln -sf "$code_dir"/kitty-tool.desktop ~/.local/share/applications/
 fi
@@ -26,5 +26,5 @@ wget https://raw.githubusercontent.com/knubie/vim-kitty-navigator/refs/heads/mas
 wget https://raw.githubusercontent.com/knubie/vim-kitty-navigator/refs/heads/master/pass_keys.py -O pass_keys.py
 
 # termpdf.py
-git clone https://github.com/dsanson/termpdf.py && \
+{ [[ -d termpdf.py ]] || git clone https://github.com/dsanson/termpdf.py; } &&
     cd termpdf.py && pip install -r requirements.txt
